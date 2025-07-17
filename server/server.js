2c071c27c644abbed5529ca2f2e6a6c4a01df153
Chakradhar Kommoju <chakrikommoju123@gmail.com>
2025-07-17 15:37:32 +0530

    Update: wildcard route

--- server/server.js
+++ server/server.js
@@ -67,7 +67,7 @@ app.use("/logout", logoutRoute);
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
